d0931e081e4da5ad6850dbbfaa91373de26f8b29
PonyM <dev@lksch.ch>
2025-09-27 10:21:06 +0200

    chore: add description to schemas
    - refactor labels that can be translated
    - add unique string for handles that are encoded in url

--- definitions/schemas.js
+++ definitions/schemas.js
@@ -14,59 +14,113 @@ export const dateStringSchema = z.string().refine(
 export const variableRegExp =
   /\{(?<type>string|number|tel|email|date)?(?::)?(?<name>[a-zA-Z]{1,})(?::)?(?<label>[a-zA-Z-. ]{0,})\}/g;
 
-export const ParagraphString = z.string().refine(
-  (val) => {
-    const braceCount = (val.match(/\{/g) || []).length;
-    const matches = [...val.matchAll(variableRegExp)];
-    return matches.length === braceCount;
-  },
-  {
-    message: "Invalid variable string in paragraph",
-  },
-);
-
-export const EventSchema = z.object({
-  handle: z.string(),
-  label: z.string(),
-  paragraphs: z.array(z.string()),
+const UniqueString = z.string().register(z.globalRegistry, {
+  id: "unique_string",
+  title: "A string that should be unique among each data type in data folder",
 });
 
-export const TypeSchema = z.object({
-  handle: z.string(),
-  label: z.string(),
-  serviceLabel: z.string(),
-  paragraphs: z.array(z.string()),
+const Label = z.string().register(z.globalRegistry, {
+  id: "label",
+  title: "A translatable label that appears on the website",
 });
+
+export const ParagraphString = z
+  .string()
+  .refine(
+    (val) => {
+      const braceCount = (val.match(/\{/g) || []).length;
+      const matches = [...val.matchAll(variableRegExp)];
+      return matches.length === braceCount;
+    },
+    {
+      message: "Invalid variable string in paragraph",
+    },
+  )
+  .register(z.globalRegistry, {
+    id: "paragraph",
+    title: "Paragraph String",
+    description:
+      "A string that is used inside the letter that may contain variable placeholders.",
+    examples: [
+      "Am {date:eventDate:Wann} habe ich von Ihnen ein Werbemail an die Adresse {email:eventEmail:E-Mail-Adresse} gesendet bekommen",
+      "Unter personenbezogene Daten sind insbesondere auch die folgenden in Ihrer Datenschutzerklärung vom {string:privacyStatementDate} aufgeführten Kategorien...",
+    ],
+  });
+
+export const EventSchema = z
+  .object({
+    handle: UniqueString,
+    label: Label,
+    paragraphs: z.array(z.string()),
+  })
+  .register(z.globalRegistry, {
+    title: "Trigger event for the data inquiry",
+    description:
+      "Event types that are displayed on the landing page as the reason for the data inquiry",
+  });
+
+export const TypeSchema = z
+  .object({
+    handle: UniqueString,
+    label: Label,
+    serviceLabel: Label,
+    paragraphs: z.array(z.string()),
+  })
+  .register(z.globalRegistry, {
+    title: "Type of Service or Service Category",
+    description:
+      "Schema for the types yml files. Used on the website to filter a company by service type encoded by the handle field",
+    examples: ["Parking", "Gastronomy", "Mobile Provider", "Address Trade"],
+  });
 
 // TODO: action is an enum, in code, valid values is removed, ...
 // models/History.js
-export const HistorySchema = z.object({
-  action: z.string(),
-  date: dateStringSchema,
-  reason: z.string(),
-});
+export const HistorySchema = z
+  .object({
+    action: z.string(),
+    date: dateStringSchema,
+    reason: z.string(),
+  })
+  .register(z.globalRegistry, {
+    title: "History Record for an Org",
+    description:
+      "History records for keep old data but mark them as removed on organization not existing anymore",
+  });
 
 // models/Bullet.js
-export const BulletsSchema = z.union([
-  z.string(), // Simple bullet
-  z.object({
-    text: z.string(),
-    get bullets() {
-      return z.array(BulletsSchema);
-    },
-  }), // Nested bullets
-]);
+export const BulletsSchema = z
+  .union([
+    z.string(), // Simple bullet
+    z.object({
+      text: z.string(),
+      get bullets() {
+        return z.array(BulletsSchema);
+      },
+    }), // Nested bullets
+  ])
+  .register(z.globalRegistry, {
+    id: "bullet",
+    title: "A bullet point",
+    description:
+      "Bullet points for inline text as either a simple string or another bullet point",
+  });
 
 // models/PrivacyStatement.js
-export const PrivacyStatementSchema = z.object({
-  paragraphs: z.array(ParagraphString).optional(),
-  variables: z
-    .object({
-      privacyStatementDate: z.string(),
-    })
-    .optional(),
-  bullets: z.array(BulletsSchema).optional(),
-});
+export const PrivacyStatementSchema = z
+  .object({
+    paragraphs: z.array(ParagraphString).optional(),
+    variables: z
+      .object({
+        privacyStatementDate: z.string(),
+      })
+      .optional(),
+    bullets: z.array(BulletsSchema).optional(),
+  })
+  .register(z.globalRegistry, {
+    title: "A companies privacy statement",
+    description:
+      "A summary of the privacy statements of a company together with an optional date when the privacy statement was in force",
+  });
 
 export const OrgType = z.enum([
   "gastro",
@@ -81,21 +135,33 @@ export const OrgType = z.enum([
 ]);
 
 // see svelte code models/org.js what is optional and what not
-export const OrgSchema = z.object({
-  name: z.string(),
-  address: z.string(),
-  types: z.array(OrgType).optional(),
-  privacyStatement: PrivacyStatementSchema.optional(),
-  // sources is not used inside the frontend code and stripped
-  sources: z.object({
-    address: z.string().url(),
-    privacyStatement: z.array(z.string().url()).optional(),
-  }),
-  history: z.array(HistorySchema).optional(),
-});
+export const OrgSchema = z
+  .object({
+    name: z.string(),
+    address: z.string(),
+    types: z.array(OrgType).optional(),
+    privacyStatement: PrivacyStatementSchema.optional(),
+    // sources is not used inside the frontend code and stripped
+    sources: z.object({
+      address: z.string().url(),
+      privacyStatement: z.array(z.string().url()).optional(),
+    }),
+    history: z.array(HistorySchema).optional(),
+  })
+  .register(z.globalRegistry, {
+    title: "Record for an organization for a yml file",
+    description:
+      "Data object containing necessary information for a company to construct letters",
+  });
 
-export const DesireSchema = z.object({
-  handle: z.string(),
-  label: z.string(),
-  paragraphs: z.array(z.string()).optional(),
-});
+export const DesireSchema = z
+  .object({
+    handle: z.string(),
+    label: z.string(),
+    paragraphs: z.array(z.string()).optional(),
+  })
+  .register(z.globalRegistry, {
+    title: "Desire schema for a yml file",
+    description:
+      "Defines the handle to appear in the url, the label that appears on the landing page and the paragraphs to render for the letter",
+  });
