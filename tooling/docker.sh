b2903b147813d47d8e7fdc382d313540aa04169a
Patrick Uhlmann <github@uhlme.ch>
2026-04-27 17:40:56 +0200

    feat: allow running yamllint from docker (#67)
    * feat: allow running yamllint from docker
    
    * feat: allow execution of docker.sh from any directory

--- tooling/docker.sh
+++ tooling/docker.sh
@@ -1,32 +1,41 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+
 CMD="${1:-}"
 
 case "$CMD" in
+  yamllint)
+    docker run --rm -it \
+      -v "$PROJECT_ROOT/:/data" \
+      cytopia/yamllint \
+      -c /data/.github/workflows/yamlconfig /data
+    ;;
   build)
     docker run --rm -it \
-      -v "$(pwd)/../:/app" \
+      -v "$PROJECT_ROOT/:/app" \
       -w /app \
       node:24-alpine \
       sh -c "npm install && npm run compile"
     ;;
   lint)
     docker run --rm -it \
-      -v "$(pwd)/../:/app" \
+      -v "$PROJECT_ROOT/:/app" \
       -w /app \
       node:24-alpine \
       sh -c "npm install && npm run lint"
     ;;
   test)
     docker run --rm -it \
-      -v "$(pwd)/../:/app" \
+      -v "$PROJECT_ROOT/:/app" \
       -w /app \
       node:24-alpine \
       sh -c "npm install && npm run lint-ci && npm run test"
     ;;
   *)
-    echo "Usage: $0 {build|test|lint}"
+    echo "Usage: $0 {yamllint|build|lint|test}"
     exit 1
     ;;
 esac
